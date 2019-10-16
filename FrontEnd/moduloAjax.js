a4eb6dbfd556149303450b40dc4bb30a590f6772
unknown <uri.arte08@gmail.com>
2019-10-16 13:15:11 -0600

    Modulo de insertar acta, incluye el local storage

---
+++ FrontEnd/moduloAjax.js
@@ -0,0 +1,6 @@
+var jsonToSend = { "Descripcion": "Papeleo 3", 
+                "considerandos": "Revisar actas pasadas",
+                "acuerdos":[{"descripcion":"Aprobar ley 101","idVotacion":1},
+                {"descripcion":"Eliminar ley 27","idVotacion":2}]}
+
+
