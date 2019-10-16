de55b81827c179f62f76c108edddb694db0a4ecf
Jaffette <jaffette@outlook.com>
2019-10-16 15:02:09 -0600

    ejemplo de modulo

--- FrontEnd/moduloAjax.js
+++ FrontEnd/moduloAjax.js
@@ -1,6 +1,23 @@
-var jsonToSend = { "Descripcion": "Papeleo 3", 
+class class_ajax
+{
+    jsonToSend
+    constructor()
+    {
+        this.jsonToSend = { "Descripcion": "Papeleo 3", 
                 "considerandos": "Revisar actas pasadas",
                 "acuerdos":[{"descripcion":"Aprobar ley 101","idVotacion":1},
                 {"descripcion":"Eliminar ley 27","idVotacion":2}]}
+
+    }
+
+    imprimir(msg) 
+    {
+        console.log(msg);    
+    }
+}
+
+var ins_ajax= new class_ajax();
+
+window.ajax=ins_ajax;
 
 
